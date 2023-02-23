0ee38d15e0c5082972d96e183d724004291da7ac
alanNunes <alan.fsa.ans@gmail.com>
2023-02-23 02:24:45 -0300

    lógica game

--- src/app/components/game-area/game-area.component.ts
+++ src/app/components/game-area/game-area.component.ts
@@ -14,16 +14,26 @@ export class GameAreaComponent implements OnInit {
   constructor() { }
   idInterval: any
   controlConnect = false
+  scoreboard = 0
   arrayPositions = [0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48]
+  arrayColors = ['green', 'blue', 'yellow', 'red']
+  pointColor: any = {
+    'green': 10,
+    'blue': 20,
+    'yellow': 30,
+    'red': 40
+  }
   currentPositionPixel = {
     x: '0%',
     y: '0%',
+    color: 'green',
     status: true
   }
 
   currentPositionPlay = {
     x: '0%',
-    y: '0%'
+    y: '0%',
+    color: 'black',
   }
 
   TIME_10_SECOUND = 1000 * 10
@@ -33,25 +43,37 @@ export class GameAreaComponent implements OnInit {
     this.connectSocket()
   }
 
-  ngOnDestroy(){
+  ngOnDestroy() {
     clearInterval(this.idInterval)
   }
 
-  changePosition(){
+  changeScreen() {
+    const elementScreen = document.getElementById('pressStart')
+    const elementDivPixel = document.getElementById('divPixel')
+    if (elementScreen) {
+      elementScreen.style.display = 'none'
+    }
+    if (elementDivPixel) {
+      elementDivPixel.style.opacity = '1'
+    }
+  }
+
+  changePosition() {
     this.idInterval = setInterval(() => {
-      console.log('ChangePosition'); 
       this.randomPosition()
       this.movePixel2()
     }, this.TIME_10_SECOUND)
   }
 
-  randomPosition(){
+  randomPosition() {
     const indexPositionX = Math.round(Math.random() * (1 - this.arrayPositions.length) + this.arrayPositions.length) - 1
     const indexPositionY = Math.round(Math.random() * (1 - this.arrayPositions.length) + this.arrayPositions.length) - 1
     const positiverOrNegativeX = Number(Math.random().toFixed(0)) ? '-' : ''
     const positiverOrNegativeY = Number(Math.random().toFixed(0)) ? '-' : ''
+    const indexColor = Math.round(Math.random() * (1 - this.arrayColors.length) + this.arrayColors.length) - 1
     this.currentPositionPixel.x = `${positiverOrNegativeX}${this.arrayPositions[indexPositionX]}%`
     this.currentPositionPixel.y = `${positiverOrNegativeY}${this.arrayPositions[indexPositionY]}%`
+    this.currentPositionPixel.color = this.arrayColors[indexColor]
   }
 
   connectSocket() {
@@ -61,27 +83,24 @@ export class GameAreaComponent implements OnInit {
 
     socket.on('actionMove', (data: any) => {
       this.actionMove(data.key)
-      console.log("actionMove ", data)
     })
 
     socket.on('actionButton', (data: any) => {
       this.changeColor(data.key)
-      console.log("actionButton ", data)
     })
 
     socket.on('startGame', (data: any) => {
       this.startGame()
-      console.log("startGame ", data)
     })
 
     socket.on('disconect', (data: any) => {
       document.getElementById('showQrCode')?.click()
-      console.log("disconect ", data)
       this.controlConnect = false
     })
   }
 
   startGame() {
+    this.changeScreen()
     this.movePixel2()
     this.changePosition()
   }
@@ -115,36 +134,44 @@ export class GameAreaComponent implements OnInit {
     this.checkMove()
   }
 
-  checkMove(){
-    if( this.currentPositionPixel.status && this.currentPositionPixel.x == this.currentPositionPlay.x && this.currentPositionPixel.y == this.currentPositionPlay.y){
+  checkMove() {
+    if (
+      this.currentPositionPixel.status
+      && this.currentPositionPixel.x == this.currentPositionPlay.x
+      && this.currentPositionPixel.y == this.currentPositionPlay.y
+      && this.currentPositionPixel.color == this.currentPositionPlay.color
+    ) {
       this.removePixel2()
     }
   }
 
-  removePixel2(){
+  removePixel2() {
     const element = document.getElementById('divPixel2')
-    if(element){
+    if (element) {
+      const elementScoreboard = document.getElementById('scoreboard')
+      if (elementScoreboard) {
+        this.scoreboard = this.scoreboard + this.pointColor[this.currentPositionPlay.color]
+        elementScoreboard.innerHTML = `${this.scoreboard}`
+      }
       element.style.opacity = '0'
       this.currentPositionPixel.status = false
     }
   }
 
-  movePixel2(){
+  movePixel2() {
     const element = document.getElementById('divPixel2')
-    if(element){
+    if (element) {
       this.currentPositionPixel.status = true
       element.style.opacity = '1'
       element.style.top = this.currentPositionPixel.y
       element.style.left = this.calcDiff()
+      element.style.backgroundColor = this.currentPositionPixel.color
     }
-
-    console.log('this.currentPositionPixel ', this.currentPositionPixel);
-    
   }
 
-  calcDiff(){
+  calcDiff() {
     let value
-    if(this.currentPositionPixel.x.includes('-')){
+    if (this.currentPositionPixel.x.includes('-')) {
       value = this.currentPositionPixel.x.split('-').join("").split("%").join("")
       return `-${(Number(value) + 1.9)}%`
     }
@@ -154,42 +181,44 @@ export class GameAreaComponent implements OnInit {
 
   moveUp(element: HTMLElement) {
     const position = Number(element.style.top.split('%')[0]) - 3
-    if(!this.checkIfCanMove(position)) return
+    if (!this.checkIfCanMove(position)) return
     element.style.top = `${position}%`
     this.currentPositionPlay.y = element.style.top
   }
 
   moveLeft(element: HTMLElement) {
     const position = Number(element.style.left.split('%')[0]) - 3
-    if(!this.checkIfCanMove(position)) return
+    if (!this.checkIfCanMove(position)) return
     element.style.left = `${position}%`
     this.currentPositionPlay.x = element.style.left
   }
 
   moveRight(element: HTMLElement) {
     const position = Number(element.style.left.split('%')[0]) + 3
-    if(!this.checkIfCanMove(position)) return
+    if (!this.checkIfCanMove(position)) return
     element.style.left = `${position}%`
     this.currentPositionPlay.x = element.style.left
   }
 
   moveDown(element: HTMLElement) {
     const position = Number(element.style.top.split('%')[0]) + 3
-    if(!this.checkIfCanMove(position)) return
+    if (!this.checkIfCanMove(position)) return
     element.style.top = `${position}%`
     this.currentPositionPlay.y = element.style.top
   }
 
-  changeColor(color: string){
+  changeColor(color: string) {
     const element = document.getElementById('divPixel')
-    if(element){
+    if (element) {
       element.style.backgroundColor = color
+      this.currentPositionPlay.color = color
+      this.checkMove()
     }
   }
 
   checkIfCanMove(value: number) {
     if (Math.abs(value) > 48) return false
-    return true    
+    return true
   }
 
 }
