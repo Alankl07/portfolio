436e42bb77bdc54aea949f5b0cc044d9faec1274
alanNunes <alan.fsa.ans@gmail.com>
2023-02-24 09:26:02 -0300

    configuração socket

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 import { environment } from 'src/environments/environment'
 
 const socket = io(environment.socketUrl, {
-    transports: ['websocket'] 
+    transports: ['websocket', 'polling'] 
 })
 
 export default socket 
